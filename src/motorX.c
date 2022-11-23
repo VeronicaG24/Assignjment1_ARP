ae114e126959de73f07d66e4f73089925e26b60c
Francesca-Corrao <s5471094@studenti.unige.it>
2022-11-23 17:32:44 +0100

    motor X update

--- src/motorX.c
+++ src/motorX.c
@@ -10,26 +10,36 @@
 #define nbytes sizeof(float)
 #define r "/tmp/fifoCX"
 #define w "/tmp/fifoXW"
+#define X_MAX 40
+#define X_MIN 0
 
 int main(){
     float v=0;
-
+    float X=X_MIN;
     while(1){
-    //aprire la pipe in letteura(CX) e contrallare non dia errore
-    int fd_read ;
-    if(open(r, O_RDONLY) !=0 ){
-        perror("Can't open /tmp/fifoCX");
-    }
-    //aprire pipe in scritture(XW)
-    int fd_write;
-    if(open(w, O_WRONLY)!=0)
-        perror("can't open  tmp/fifoXW");
-    //leggere CX e controllare che non dia errore
-    if(read(fd_read,&v, nbytes)==-1)
-        perror("error in reading");
-    //aggiornare X
-
-    //scrivere in XW
-    //sleep
-    }
+        //aprire la pipe in letteura(CX) e contrallare non dia errore
+        int fd_read ;
+        if(open(r, O_RDONLY) !=0 ){
+            perror("Can't open /tmp/fifoCX");
+        }
+        //aprire pipe in scritture(XW)
+        int fd_write;
+        if(open(w, O_WRONLY)!=0)
+            perror("can't open  tmp/fifoXW");
+        //leggere CX e controllare che non dia errore
+        if(read(fd_read,&v, nbytes)==-1)
+            perror("error in reading");
+        //aggiornare X
+        float dx = (v*dt);
+        if(X<X_MAX-dx){
+            X+=dx;
+        }
+        else 
+            X=X_MAX;
+        //scrivere in XW
+        if(write(fd_write,&X, nbytes)==-1)
+            perror("error in writing");
+        //sleep
+        sleep(1);
+        }
 }
