5800bf99ace09d75b547947fffaa9ff893dcc05d
Veronica <veronicagavagna.vg@gmail.com>
2022-12-17 17:44:32 +0100

    comments and refactor watchdog

--- src/watchdog.c
+++ src/watchdog.c
@@ -8,7 +8,7 @@ AUTHOR: Written by Francesca Corrao and Veronica Gavagna.
 -----------------------------------------------------------------------------
 
 DESCRIPTION
-  	The Warchdog process checks the inactivity of the program 
+  	The Warchdog process checks the inactivity of the programs 
     though the log file. If any process is inactive for more than 60 seconds
     it kills all the process including itself.
 
@@ -23,7 +23,11 @@ DESCRIPTION
 #include <signal.h>
 #include <time.h>
 
-// Retrieve current time procedure
+/*=====================================
+  Get current time
+  RETURN:
+    time and date
+=====================================*/
 char* current_time(){
     time_t rawtime;
     struct tm * timeinfo;
@@ -36,8 +40,14 @@ char* current_time(){
     return timedate;
 }
 
-int main(int argc, char const *argv[])
-{
+/*=====================================
+  Checks the inactivity of the programs
+  RETURN:
+    null
+=====================================*/
+int main(int argc, char const *argv[]) {
+  
+    //pids of all the processes
     pid_t pid_motorX, pid_motorZ,pid_cmd, pid_insp, pid_world, pid_master;
     pid_motorX = atoi(argv[1]);
     pid_motorZ = atoi(argv[2]);
@@ -46,70 +56,85 @@ int main(int argc, char const *argv[])
     pid_world=atoi(argv[5]);
     pid_master=atoi(argv[6]);
 
-    //change into watchdog
-    FILE *flog; //file pointer log file
+    //varibles for log file
+    FILE *flog;
     long bytesWritten = 0;
     long bytesWritten_old = 0;
     time_t startTimer;
     sleep(1);
 
+    //infinite loop
     while(1) { 
-    int status;
-    //waitpid(pid_cmd, &status, 0);
-    //waitpid(pid_insp, &status, 0);
-    flog = fopen("./logFile.log", "r"); //decidere posizione
-    fseek(flog, 0L, SEEK_END); //mi posizione alla fine del file (file pointer at 0 bytes from the end)
-    bytesWritten_old = ftell(flog); //numero di byte dall'inizio alla fine (alla posizione del file pointer)
-    fclose(flog);
-    startTimer = time(NULL);
+      int status;
+      
+      //open log file
+      flog = fopen("./logFile.log", "r");
+      //file pointer at 0 bytes from the end
+      fseek(flog, 0L, SEEK_END);
+      //count number of bytes written before the file pointer
+      bytesWritten_old = ftell(flog);
+      //close log file
+      fclose(flog);
+      //set timer
+      startTimer = time(NULL);
+      //set number of bytes written new to the one read before
+      bytesWritten = bytesWritten_old;
 
-    bytesWritten = bytesWritten_old;
-    while((bytesWritten==bytesWritten_old) && (difftime(time(NULL), startTimer) < 60)) {
-        flog = fopen("./logFile.log", "r"); //decidere posizione
+      //loop until the number of bytes before and after are equal AND the timer of 60 seconds is not over
+      while((bytesWritten==bytesWritten_old) && (difftime(time(NULL), startTimer) < 60)) {
+        //open log fine
+        flog = fopen("./logFile.log", "r");
+        //file pointer at 0 bytes from the end
         fseek(flog, 0L, SEEK_END); 
-        bytesWritten_old = ftell(flog); 
+        //update old number of bytes
+        bytesWritten_old = ftell(flog);
+        //close the file
         fclose(flog);
-    //sleep
-  }
+      }
 
+      //if timer is over, so the number of bytes is not changed, kill everything 
+      if(difftime(time(NULL), startTimer) >= 60) {
+        //write on the log file
+        char * currTime = current_time();
+        flog = fopen("./logFile.log", "a+");
+        fprintf(flog, "< WATCHDOG > inactivity detected, resetting the system at: %s \n", currTime);
+        fclose(flog);
 
-  if(difftime(time(NULL), startTimer) >= 60) {
-    // Write on the LOG.log
-    char * currTime = current_time();
-    flog = fopen("./logFile.log", "a+"); //decidere posizione (controlla a o a+, per me a+ meglio)
-    fprintf(flog, "< WATCHDOG > inactivity detected, resetting the system at: %s \n", currTime);
-    fclose(flog);
+        //kill motorX process
+        sleep(1);
+        if(kill(pid_motorX,SIGINT) == -1) {
+          perror("Watchdog: failed to kill motorX");
+        }
+        //kill motorZ process
+        sleep(1);
+        if(kill(pid_motorZ,SIGINT) == -1) { 
+          perror("Watchdog: failed to kill motorZ");
+        }
+        //kill world process
+        sleep(1);
+        if(kill(pid_world,SIGINT) == -1) {
+          perror("Watchdog: failed to kill world");
+        }
+        //kill command console process
+        sleep(1);
+        if(kill(pid_cmd,SIGINT) == -1) {
+          perror("Watchdog: failed to kill motorX");
+        }
+        //kill inspection console process
+        sleep(1);
+        if(kill(pid_insp,SIGINT) == -1) {
+          perror("Watchdog: failed to kill motorX");
+        }
+        sleep(1);
+        //kill master process
+        if(kill(pid_master,SIGINT) == -1) {
+          perror("Watchdog: failed to kill master");
+        }
+        sleep(1);
 
-    sleep(1);
-    if(kill(pid_motorX,SIGINT) == -1) { //controlla sia -1
-      perror("Watchdog: failed to kill motorX");
+        //kill itself
+        printf ("Watchdog: exiting with status %d\n", status);
+        exit(0);
+      }
     }
-    sleep(1); //simone ha consigliato di fare le sleep se no non killa bene
-    if(kill(pid_motorZ,SIGINT) == -1) { 
-      perror("Watchdog: failed to kill motorZ");
-    }
-    sleep(1);
-    if(kill(pid_world,SIGINT) == -1) {
-      perror("Watchdog: failed to kill world");
-    }
-    sleep(1);
-    if(kill(pid_cmd,SIGINT) == -1) { //controlla sia -1
-      perror("Watchdog: failed to kill motorX");
-    }
-    sleep(1);
-    if(kill(pid_insp,SIGINT) == -1) { //controlla sia -1
-      perror("Watchdog: failed to kill motorX");
-    }
-    sleep(1);
-    if(kill(pid_master,SIGINT) == -1) { //controlla sia -1
-      perror("Watchdog: failed to kill master");
-    }
-    sleep(1);
-
-    //unlinkpipe();
-    printf ("Watchdog: exiting with status %d\n", status);
-    exit(0);
-  }
-    
-}
 }
